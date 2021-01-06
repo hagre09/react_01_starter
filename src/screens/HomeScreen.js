fdb994f8105903f13c02daecb1fe7dd59ce8d11a
Emadeldeen Abdallah <emad_566@hotmail.com>
2021-01-06 17:00:24 +0200

    first commit

---
+++ src/screens/HomeScreen.js
@@ -0,0 +1,34 @@
+import React from "react";
+import { Text, StyleSheet, View, Button, TouchableOpacity } from "react-native";
+
+const HomeScreen = ({navigation}) => {
+  return (
+    <View>
+      <Text style={styles.text}>EHm@@gdr</Text>
+      <Button
+        onPress={()=> navigation.navigate('Components')}
+        title="Components"
+      />
+
+      <Button
+        title="List"
+        onPress={()=> navigation.navigate('List')}
+      />
+
+      <Button
+        title="Image"
+        onPress={()=> navigation.navigate('Image')}
+      />
+
+    </View>
+  ); 
+};
+
+const styles = StyleSheet.create({
+  text: {
+    fontSize: 30
+  }
+});
+
+export default HomeScreen;
+
